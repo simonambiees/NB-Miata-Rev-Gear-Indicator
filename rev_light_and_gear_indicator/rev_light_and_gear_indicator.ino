9fc2e1560e2350fa861b3a5c5aa5cd3f31d3bdb6
Simon Jin <1115443660@qq.com>
2024-04-28 02:11:35 -0400

    tuned rev limits

--- rev_light_and_gear_indicator/rev_light_and_gear_indicator.ino
+++ rev_light_and_gear_indicator/rev_light_and_gear_indicator.ino
@@ -14,14 +14,14 @@
 #define RPM_PER_HZ 30
 
 // Cruise Mode threshold time in loop count
-#define CRUISE_THRESHOLD 350
+#define CRUISE_THRESHOLD 250
 // Cruise Mode limit RPMs
 #define CRUISE_LOW 3000
 #define CRUISE_HIGH 5000
 volatile int cruise_count = 0;
 
 // RPM bar limit RPMs
-#define RPM_LOW 3500
+#define RPM_LOW 4000
 
 // Shift Flashing Warning threshold RPM
 #define RPM_FLASH_THRESHOLD 5800
